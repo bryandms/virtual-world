6ff7b177cc1f53c34154f299d9f8c58fa3ef109c
Daniel fonseca Solano <dfonse11@gmail.com>
2019-07-23 23:40:04 -0600

    Mandar Mensajes
    Se ve el ultimo mensaje que manda cada jugador

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import Message from './Message';
 
 class Player extends Component {
+	
+	constructor(props) {
+        super(props);
+		this.handleEnter = this.handleEnter.bind(this);
+		this.ultimateMessage="";
+		
+    }
 	render() {
 		const { style } = this.props;
 
 		return (
+			<div>
+		
+
 			<div style={style}>
-				Player
+			
+			<Message textMessage= {this.ultimateMessage} />
+			<input id="message" type="text" placeholder="Mensaje" onKeyDown={this.handleEnter} />
+			<h1>Player</h1>		
 			</div>
+			</div>
+			
 		)
+		
 	}
+	 handleEnter(e) {
+		if (e.key === 'Enter') {
+			this.ultimateMessage=e.target.value;
+		}
+	  }
+	  
 }
 
 export default Player;
